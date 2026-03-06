9c780606639474a28e56457b03675c61660e0bc5
Anil Kumar <146966464+justanilkumar@users.noreply.github.com>
2026-03-06 13:51:08 +0530

    Time: 0 ms (100%), Space: 21.4 MB (66%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root == NULL) return false;
+        if(root == NULL)
+            return false;
 
         if(root->left == NULL && root->right == NULL)
             return targetSum == root->val;
