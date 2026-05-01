f0c894e8bb9fb12ba15847cd19162f75cfdf8380
Anil Kumar <146966464+justanilkumar@users.noreply.github.com>
2026-05-01 10:59:02 +0530

    Time: 0 ms (100%), Space: 99.5 MB (79.08%) - LeetHub

---
+++ 0396-rotate-function/0396-rotate-function.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+// formula: F(k) = F(k-1) + sum - n*nums[n-k]
+    int maxRotateFunction(vector<int>& arr) {
+        int ans=0 , f=0 , sum=0 , n=arr.size();
+        for (int i=0 ; i<n ; i++){
+            f+=(i*arr[i]);
+            sum+=arr[i];
+        }
+        ans=f;
+        for (int i=1 ; i<n ; i++){
+            f=f+sum-(n*arr[n-i]);
+            ans=max(ans,f);
+        }
+        return ans;
+    }
+};
